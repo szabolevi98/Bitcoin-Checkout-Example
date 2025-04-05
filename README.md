ae1536efe4f6acd7b180e9fec8ad012bf3487a2e
Levente <Levente@DESKTOP-JPGKIQC>
2025-04-06 00:51:19 +0200

    Edit readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Bitcoin-Checkout-Example
 ![Screenshot](screenshot.png?raw=true "Bitcoin-Checkout-Example")  
 An example Bitcoin Payment Checkout using the **blockchain.info** public api.
+
+- Test address: `05fd3701beb9158927de4ff168fa7a30b2a19b2e0e865f8a59f8ad275de999a5`
+- Test transaction hash: `05fd3701beb9158927de4ff168fa7a30b2a19b2e0e865f8a59f8ad275de999a5` 
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'btc_address' => '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', // Test transaction hash: 05fd3701beb9158927de4ff168fa7a30b2a19b2e0e865f8a59f8ad275de999a5
+    'btc_address' => '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
     'currency' => 'EUR',
     'amount' => 5,
     'threshold' => 0.98, // We are accepting 98%+ of the current amount since price is dynamic
